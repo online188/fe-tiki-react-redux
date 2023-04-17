47f7615ddd93034df5d0c0ac648fd03a4366aaa8
Hoang Nam <alphalinkmedia@gmail.com>
2023-04-18 05:29:15 +0700

    Fixed CORS

--- src/services/authService.js
+++ src/services/authService.js
@@ -4,138 +4,137 @@ import { path } from 'utils';
 
 // Register
 const registerAcc = (data) => {
-    return axios.post(`${path.PORT}/auth/register`, data)
-} 
+    return axios.post(`${path.PORT}/auth/register`, data);
+};
 
 // Login
 const loginAcc = (data) => {
-    return axios.post(`${path.PORT}/auth/login`, data)
-}
+    return axios.post(`${path.PORT}/auth/login`, data, { withCredentials: true }); //Phải thêm cái này thì mới setCookie
+};
 
 // Logout
 const logout = () => {
-    return axios.delete(`${path.PORT}/auth/logout`)
-}
+    return axios.delete(`${path.PORT}/auth/logout`);
+};
 
 // get user
 const GetUser = () => {
     return instance.get(`${path.PORT}/user`, {
         headers: {
-            'Authorization': `Bearer ${localStorage.getItem('token')}`
-        }
-    })
-}
+            Authorization: `Bearer ${localStorage.getItem('token')}`,
+        },
+    });
+};
 
 // Verify token
 const verifyToken = () => {
-    return axios.get(`${path.PORT}/auth/token`,{
+    return axios.get(`${path.PORT}/auth/token`, {
         headers: {
-            'Authorization': `Bearer ${localStorage.getItem('token')}`
-        }
+            Authorization: `Bearer ${localStorage.getItem('token')}`,
+        },
     });
-}
+};
 
 // update user
 const updateUser = (data) => {
     return axios.put(`${path.PORT}/update-user`, data, {
         headers: {
-            'Authorization': `Bearer ${localStorage.getItem('token')}`
-        }
+            Authorization: `Bearer ${localStorage.getItem('token')}`,
+        },
     });
-}
+};
 
 // change password
-const ChangePass= (data) => {
+const ChangePass = (data) => {
     return instance.put(`/change-password`, data, {
         headers: {
-            'Authorization': `Bearer ${localStorage.getItem('token')}`
-        }
+            Authorization: `Bearer ${localStorage.getItem('token')}`,
+        },
     });
-}
+};
 
 // get all notify
 const getAllNotify = (userId) => {
     return instance.get(`/notify?userId=${userId}`, {
         headers: {
-            'Authorization': `Bearer ${localStorage.getItem('token')}`
-        }
+            Authorization: `Bearer ${localStorage.getItem('token')}`,
+        },
     });
-}
+};
 
 // get notify unread
 const getNotify = (userId, status) => {
     return instance.get(`/notify?userId=${userId}&status=${status}`, {
         headers: {
-            'Authorization': `Bearer ${localStorage.getItem('token')}`
-        }
+            Authorization: `Bearer ${localStorage.getItem('token')}`,
+        },
     });
-}
+};
 
 //update status notify
 const updateStatusNotify = (data) => {
-    return instance.put(`/update-notify`,data, {
+    return instance.put(`/update-notify`, data, {
         headers: {
-            'Authorization': `Bearer ${localStorage.getItem('token')}`
-        }
+            Authorization: `Bearer ${localStorage.getItem('token')}`,
+        },
     });
-}
+};
 
 // mark all notify as read
 const MarkAllNotifyAsRead = (data) => {
-    return instance.put(`/mark-all-as-read`,data, {
+    return instance.put(`/mark-all-as-read`, data, {
         headers: {
-            'Authorization': `Bearer ${localStorage.getItem('token')}`
-        }
+            Authorization: `Bearer ${localStorage.getItem('token')}`,
+        },
     });
-}
+};
 
 // filterNotify
 const filterNotify = (userId, type) => {
     return instance.get(`/notify?userId=${userId}&type=${type}`, {
         headers: {
-            'Authorization': `Bearer ${localStorage.getItem('token')}`
-        }
+            Authorization: `Bearer ${localStorage.getItem('token')}`,
+        },
     });
-}
+};
 
 // Rating product
 const RatingProduct = (data) => {
     return instance.post(`/rating`, data, {
         headers: {
-            'Authorization': `Bearer ${localStorage.getItem('token')}`
-        }
+            Authorization: `Bearer ${localStorage.getItem('token')}`,
+        },
     });
-}
+};
 
 // get point user
 const getPointUser = (userId) => {
     return instance.get(`/point-user?userId=${userId}`, {
         headers: {
-            'Authorization': `Bearer ${localStorage.getItem('token')}`
-        }
+            Authorization: `Bearer ${localStorage.getItem('token')}`,
+        },
     });
-}
+};
 
 // save discount user
 const saveDiscount = (data) => {
     return instance.post(`/save-discount`, data, {
         headers: {
-            'Authorization': `Bearer ${localStorage.getItem('token')}`
-        }
+            Authorization: `Bearer ${localStorage.getItem('token')}`,
+        },
     });
-}
+};
 
 // get discount user
 const getDiscountUser = (userId) => {
     return instance.get(`/get-discount-user?userId=${userId}`, {
         headers: {
-            'Authorization': `Bearer ${localStorage.getItem('token')}`
-        }
+            Authorization: `Bearer ${localStorage.getItem('token')}`,
+        },
     });
-}
+};
 
-
-export{
+export {
     registerAcc,
     loginAcc,
     logout,
@@ -151,5 +150,5 @@ export{
     RatingProduct,
     getPointUser,
     saveDiscount,
-    getDiscountUser
-}
+    getDiscountUser,
+};
